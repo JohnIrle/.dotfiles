94e00eea238400535585c011c147e801b863824a
John Irle <irlejohn@gmail.com>
2021-03-17 01:55:32 -0500

    Add compe

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,3 +1,6 @@
 require("plugins.telescope")
 require("plugins.devicons")
 require("plugins.plug-colorizer")
+require("plugins.compe")
+
+require'nvim-treesitter.configs'.setup {highlight = {enable = true}}
