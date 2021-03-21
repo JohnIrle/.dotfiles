09015f3ea8b9d1a208e95f53f060ba9b1d78abab
John Irle <irlejohn@gmail.com>
2021-03-20 19:04:53 -0500

    add background switching on osx

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -25,4 +25,43 @@ M.search_dotfiles = function()
   })
 end
 
+local function set_background(content)
+  vim.fn.system("osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file \"" .. content .. "\"\'")
+end
+
+local function select_background(prompt_bufnr, map)
+  local function set_the_background(close)
+    local content = require('telescope.actions.state').get_selected_entry(prompt_bufnr)
+    set_background(content.cwd .. "/" .. content.value)
+    if close then require('telescope.actions').close(prompt_bufnr) end
+  end
+
+  map('i', '<C-b>', function()
+    set_the_background()
+  end)
+
+  map('i', '<CR>', function()
+    set_the_background(true)
+  end)
+end
+
+local function image_selector(prompt, cwd)
+  return function()
+    require("telescope.builtin").find_files({
+      prompt_title = prompt,
+      cwd = cwd,
+      previewer = false,
+      attach_mappings = function(prompt_bufnr, map)
+        select_background(prompt_bufnr, map)
+
+        -- Please continue mapping (attaching additional key maps):
+        -- Ctrl+n/p to move up and down the list.
+        return true
+      end
+    })
+  end
+end
+
+M.anime_selector = image_selector("< Background Selector > ", "~/Pictures/gruvbox")
+
 return M
