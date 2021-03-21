ce978c86421b89cb36bcc306f5746655474cc8c9
John Irle <irlejohn@gmail.com>
2021-03-20 19:03:36 -0500

    add gitsigns and lightbulb

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,5 +2,7 @@ require("plugins.telescope")
 require("plugins.devicons")
 require("plugins.plug-colorizer")
 require("plugins.compe")
+require("plugins.gitsigns")
+require("plugins.nvim-lightbulb")
 
 require'nvim-treesitter.configs'.setup {highlight = {enable = true}}
