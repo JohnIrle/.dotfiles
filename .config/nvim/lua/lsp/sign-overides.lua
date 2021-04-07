6f13a22c0e3ab9f845e094ae2619a423636c493d
John Irle <irlejohn@gmail.com>
2021-04-07 10:40:20 -0500

    add markdown and overides for lsp status icons

---
+++ .config/nvim/lua/lsp/sign-overides.lua
@@ -0,0 +1,4 @@
+vim.cmd([[sign define LspDiagnosticsSignError text=❌ texthl=LspDiagnosticsSignError linehl= numhl=]])
+vim.cmd([[sign define LspDiagnosticsSignWarning text=⚠️ texthl=LspDiagnosticsSignWarning linehl= numhl=]])
+vim.cmd([[sign define LspDiagnosticsSignInformation text=🔔 texthl=LspDiagnosticsSignInformation linehl= numhl=]])
+vim.cmd([[sign define LspDiagnosticsSignHint text=👀 texthl=LspDiagnosticsSignHint linehl= numhl=]])
