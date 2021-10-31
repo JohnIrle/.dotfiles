cfc396ef7a92d9693be1e5561cef286470cbac4f
John Irle <irlejohn@gmail.com>
2021-10-30 21:39:49 -0500

    update autopairs mapping and clean up tabselection

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,13 +1,8 @@
+local lspkind = require 'lspkind'
+lspkind.init()
+
 local cmp = require 'cmp'
-
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-  local col = vim.fn.col('.') - 1
-  return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
-end
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 
 cmp.setup {
   snippet = {
@@ -15,36 +10,35 @@ cmp.setup {
       vim.fn['vsnip#anonymous'](args.body)
     end
   },
-  sources = {{name = 'nvim_lsp'}, {name = 'buffer'}, {name = "nvim_lua"}, {name = "vsnip"}},
+  sources = {{name = "gh_issues"}, {name = "nvim_lua"}, {name = 'nvim_lsp'}, {name = "vsnip"}, {name = 'buffer', keyword_lenth = 5}},
   mapping = {
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-e>'] = cmp.mapping.close(),
     ['<C-Space>'] = cmp.mapping.complete({behavior = cmp.ConfirmBehavior.Replace, select = true}),
-    ['<Tab>'] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(t '<C-n>', 'n')
-      elseif check_back_space() then
-        vim.fn.feedkeys(t '<Tab>', 'n')
-      else
-        fallback()
-      end
-    end,
-    ['<S-Tab>'] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(t '<C-p>', 'n')
-      else
-        fallback()
-      end
-    end
+    ['<CR>'] = cmp.mapping.confirm()
+    -- ["<Tab>"] = function(fallback)
+    --   if cmp.visible() then
+    --     cmp.select_next_item()
+    --   else
+    --     fallback()
+    --   end
+    -- end,
+    -- ["<S-Tab>"] = function(fallback)
+    --   if cmp.visible() then
+    --     cmp.select_prev_item()
+    --   else
+    --     fallback()
+    --   end
+    -- end,
   },
-  formatting = {
-    format = function(entry, vim_item)
-      -- icons
-      vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
 
-      vim_item.menu = ({buffer = "[Buffer]", nvim_lsp = "[LSP]", nvim_lua = "[Lua]"})[entry.source.name]
-      return vim_item
-    end
+  formatting = {
+    format = lspkind.cmp_format {
+      with_text = true,
+      menu = {buffer = "[buf]", nvim_lsp = "[LSP]", nvim_lua = "[api]", path = "[path]", vsnip = "[snip]", gh_issues = "[issues]"}
+    }
   }
 }
 
-require('nvim-autopairs.completion.cmp').setup({map_cr = true, map_complete = true, auto_select = true})
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({map_char = {tex = ''}}))
