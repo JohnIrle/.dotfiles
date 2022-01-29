7ac0044086b8f4a07c40a05b9054d01ba1141f67
John Irle <irlejohn@gmail.com>
2022-01-28 22:12:17 -0600

    update layout and add file browser

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,9 +2,23 @@ local actions = require('telescope.actions')
 require('telescope').setup {
   defaults = {
     file_sorter = require('telescope.sorters').get_fzy_sorter,
-    prompt_prefix = ' >',
+    prompt_prefix = '> ',
+    selection_caret = "  ",
+    entry_prefix = "  ",
     color_devicons = true,
-
+    winblend = 0,
+    sorting_strategy = "ascending",
+    layout_strategy = "horizontal",
+    layout_config = {
+      horizontal = {prompt_position = "top", preview_width = 0.55, results_width = 0.8},
+      vertical = {mirror = false},
+      width = 0.87,
+      height = 0.80,
+      preview_cutoff = 120
+    },
+    border = {},
+    use_less = true,
+    set_env = {["COLORTERM"] = "truecolor"}, -- default = nil,
     file_previewer = require('telescope.previewers').vim_buffer_cat.new,
     grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
     qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
@@ -15,6 +29,7 @@ require('telescope').setup {
 }
 
 require('telescope').load_extension('fzy_native')
+require('telescope').load_extension('file_browser')
 
 local M = {}
 M.search_dotfiles = function()
--- .config/nvim/plug-config/telescope.vim
+++ .config/nvim/plug-config/telescope.vim
@@ -1,12 +1,13 @@
 lua require("plugins")
 
 nnoremap <leader>ps :lua require('telescope.builtin').grep_string({ search = vim.fn.input("Grep For > ")})<CR>
-nnoremap <leader>pf :lua require('telescope.builtin').git_files()<CR>
-nnoremap <C-p> :lua require('telescope.builtin').find_files()<CR>
+nnoremap <leader>pf :lua require('telescope.builtin').find_files()<CR>
+nnoremap <C-p> :lua require('telescope.builtin').git_files()<CR>
 
 nnoremap <leader>pw :lua require('telescope.builtin').grep_string { search = vim.fn.expand("<cword>") }<CR>
 nnoremap <leader>pb :lua require('telescope.builtin').buffers()<CR>
 nnoremap <leader>ph :lua require('telescope.builtin').help_tags()<CR>
-nnoremap <leader>pe :lua require('telescope.builtin').file_browser()<CR>
+nnoremap <leader>dl :Telescope diagnostics<CR>
+nnoremap <leader>pe :Telescope file_browser<CR>
 nnoremap <leader>vrc :lua require('plugins.telescope').search_dotfiles()<CR>
-nnoremap <leader>va  :lua require('plugins.telescope').anime_selector()<CR>
+nnoremap <leader>va :lua require('plugins.telescope').anime_selector()<CR>
