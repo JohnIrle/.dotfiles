fba884c4634f99b8553d5ec5f95619704e729688
John Irle <irlejohn@gmail.com>
2024-03-03 13:20:08 -0600

    format with stylua

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
-require'nvim-treesitter.configs'.setup {
+require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "javascript", "typescript", "c", "lua", "rust" },
+  ensure_installed = { 'vim', 'vimdoc', 'bash', 'html', 'markdown', 'javascript', 'typescript', 'c', 'lua', 'rust' },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -19,4 +19,5 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+  indent = { enable = true },
 }
