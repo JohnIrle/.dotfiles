0540870839865204233aae28f4682d4ce94a39ac
John Irle <irlejohn@gmail.com>
2024-03-03 13:13:45 -0600

    setup after lspzero update. use lua ls instead of sumneko

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,68 +1,91 @@
 require('lspkind').init()
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
+local lsp = require 'lsp-zero'
+lsp.preset 'recommended'
 
-lsp.ensure_installed({'tsserver', 'eslint', 'sumneko_lua', 'rust_analyzer'})
+local ensure_installed = { 'tsserver', 'eslint', 'lua_ls', 'rust_analyzer', 'stylua' }
 
-lsp.configure('sumneko_lua', {
+require('mason').setup {}
+require('mason-lspconfig').setup {
+  handlers = {
+    lsp.default_setup,
+  },
+}
+require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+lsp.configure('lua_ls', {
   settings = {
     Lua = {
       runtime = {
         -- Tell the language server which version of Lua you are using. LuaJIT for NVIM
         version = 'LuaJIT',
         -- Setup your lua path
-        path = vim.split(package.path, ';')
+        path = vim.split(package.path, ';'),
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = {'vim', 'describe', 'it', 'R', 'P'}
+        globals = { 'vim', 'describe', 'it', 'R', 'P' },
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
-        library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true}
-      }
-    }
-  }
+        library = {
+          [vim.fn.expand '$VIMRUNTIME/lua'] = true,
+          [vim.fn.expand '$VIMRUNTIME/lua/vim/lsp'] = true,
+        },
+      },
+    },
+  },
+})
+
+require('rust-tools').setup()
+
+lsp.configure('rust_analyzer', {
+  cmd = {
+    'rustup',
+    'run',
+    'stable',
+    'rust-analyzer',
+  },
 })
 
 local lspkind = require 'lspkind'
 lspkind.init()
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require 'cmp'
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
 
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-  ['<C-f>'] = cmp.mapping.scroll_docs(4),
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-e>'] = cmp.mapping.close(),
-  ['<C-Space>'] = cmp.mapping.complete({behavior = cmp.ConfirmBehavior.Replace, select = true}),
-  ['<CR>'] = cmp.mapping.confirm()
-})
-
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
-
-lsp.set_preferences({sign_icons = {error = '❌', hint = '👀', info = '🔔'}})
-
-lsp.setup_nvim_cmp({
-  mapings = cmp_mappings,
+cmp.setup {
+  mapping = {
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<C-Space>'] = cmp.mapping.complete { behavior = cmp.ConfirmBehavior.Replace, select = true },
+    ['<CR>'] = cmp.mapping.confirm(),
+  },
   formatting = {
     format = lspkind.cmp_format {
       with_text = true,
-      menu = {buffer = "[buf]", nvim_lsp = "[LSP]", nvim_lua = "[api]", path = "[path]", luasnip = "[snip]", gh_issues = "[issues]"}
-    }
+      menu = {
+        buffer = '[buf]',
+        nvim_lsp = '[LSP]',
+        nvim_lua = '[api]',
+        path = '[path]',
+        luasnip = '[snip]',
+        gh_issues = '[issues]',
+      },
+    },
   },
-  experimental = {native_menu = false, ghost_text = true}
-})
+  experimental = { native_menu = false, ghost_text = true },
+}
 
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({map_char = {tex = ''}}))
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done { map_char = { tex = '' } })
 
 local function documentHighlight(client)
   -- Set autocommands conditional on server_capabilities
   if client and client.server_capabilities.documentHighlightProvider then
-    vim.api.nvim_exec([[
+    vim.api.nvim_exec(
+      [[
     hi LspReferenceRead cterm=bold ctermbg=red guibg=#504945
     hi LspReferenceText cterm=bold ctermbg=red guibg=#504945
     hi LspReferenceWrite cterm=bold ctermbg=red guibg=#504945
@@ -71,25 +94,32 @@ local function documentHighlight(client)
       autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
       autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
     augroup END
-  ]], false)
+  ]],
+      false
+    )
   end
 end
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
 
-  documentHighlight(client, bufnr)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-  vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, opts)
-  vim.keymap.set("n", "<leader>h", vim.lsp.buf.signature_help, opts)
-  vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-  vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-  vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-  vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-  vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+  documentHighlight(client)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', '<leader>vws', vim.lsp.buf.workspace_symbol, opts)
+  vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', '<leader>h', vim.lsp.buf.signature_help, opts)
+  vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, opts)
+  vim.keymap.set('n', '<leader>vrn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, opts)
+  vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, opts)
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
 end)
 
 lsp.setup()
+
+-- Enable inline dianostic messages
+vim.diagnostic.config {
+  virtual_text = true,
+}
