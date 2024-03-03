0a955eb11dd173744faee550200a67eb44c6455d
John Irle <irlejohn@gmail.com>
2024-03-03 13:21:36 -0600

    add conform and format

--- .config/nvim/lua/john/lazy.lua
+++ .config/nvim/lua/john/lazy.lua
@@ -1,20 +1,17 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
--- Only required if you have packer configured as `opt`
--- vim.cmd [[packadd packer.nvim]]
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
-  })
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
-return require('lazy').setup({
+return require('lazy').setup {
 
   'alvan/vim-closetag',
   'leafgarland/typescript-vim',
@@ -28,8 +25,6 @@ return require('lazy').setup({
 
   'nanotee/luv-vimdocs',
   'milisims/nvim-luaref',
-  'nkrkv/nvim-treesitter-rescript',
-  'rescript-lang/vim-rescript',
 
   -- Styles
   'morhetz/gruvbox',
@@ -46,25 +41,63 @@ return require('lazy').setup({
   'kosayoda/nvim-lightbulb',
   'williamboman/mason.nvim',
   'williamboman/mason-lspconfig.nvim',
-  
+
   'VonHeikemen/lsp-zero.nvim',
-  'neovim/nvim-lspconfig', 
-  'hrsh7th/nvim-cmp', 
-  'hrsh7th/cmp-buffer', 
-  'hrsh7th/cmp-path', 
+  'neovim/nvim-lspconfig',
+  'hrsh7th/nvim-cmp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
   'saadparwaiz1/cmp_luasnip',
   'hrsh7th/cmp-nvim-lsp',
   'hrsh7th/cmp-nvim-lua', -- Snippets
-  'L3MON4D3/LuaSnip', -- Snippet Collection (Optional)
+  'WhoIsSethDaniel/mason-tool-installer.nvim',
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    opts = {
+      notify_on_error = false,
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use a sub-list to tell conform to run *until* a formatter
+        -- is found.
+        -- javascript = { { "prettierd", "prettier" } },
+      },
+    },
+  },
+  {
+    'L3MON4D3/LuaSnip',
+    -- follow latest release.
+    version = 'v2.*', -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = 'make install_jsregexp',
+  },
   'rafamadriz/friendly-snippets',
 
   -- Telescope
-  'nvim-lua/popup.nvim',
-  'nvim-lua/plenary.nvim',
-  'nvim-telescope/telescope.nvim',
-  'nvim-telescope/telescope-fzy-native.nvim',
+  {
+    'nvim-telescope/telescope.nvim',
+    event = 'VimEnter',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-lua/popup.nvim',
+      {
+        'nvim-telescope/telescope-fzy-native.nvim',
+        build = make,
 
-  {'nvim-treesitter/nvim-treesitter', build = 'TSUpdate'},
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+    },
+  },
+
+  { 'nvim-treesitter/nvim-treesitter', build = 'TSUpdate' },
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
   'tpope/vim-commentary',
@@ -73,8 +106,4 @@ return require('lazy').setup({
   'vim-utils/vim-man',
   'christoomey/vim-tmux-navigator',
   'christoomey/vim-system-copy',
-  -- use 'dstein64/vim-startuptime'
-
-  -- use '~/Developer/JohnIrle/stackmaps.nvim/'
-
-})
+}
